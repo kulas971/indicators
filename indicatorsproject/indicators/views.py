99b11af309f2b618f1cf981d15c80a632fde164b
kulas971 <rehan.mateusz@gmail.com>
2020-12-22 17:11:17 +0100

    add UploadView

--- indicatorsproject/indicators/views.py
+++ indicatorsproject/indicators/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import requests as rq
 
-# Create your views here.
+from django.views.generic import TemplateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from .forms import UploadFileForm
+
+class UploadView(LoginRequiredMixin, FormView):
+
+    form_class = UploadFileForm
+    template_name = "indicators/upload.html"
+
+    def get_success_url(self):
+        file = self.request.FILES['file'].read()
+        client = rq.session()
+        client.get('https://e-sprawozdania.biz.pl/show/?lang=pl')
+        csrftoken = client.cookies['csrftoken']
+        payload = {'csrfmiddlewaretoken':csrftoken}
+
+        response = client.post('https://e-sprawozdania.biz.pl/show/',
+            files={'document':file}, data=payload)
+
+        source = response.content.decode()
+
+        return reverse_lazy('indicators:upload')
+
+    def get_context_data(self, **kwargs):
+        context = super(UploadView, self).get_context_data(**kwargs)
+        return context
